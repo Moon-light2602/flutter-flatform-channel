3360d2765eb256c6dab2dfbfca5343d275726766
duyenntm <dyndyn2602@gmail.com>
2023-11-20 16:28:48 +0700

    flutter flatform channel

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_flatform_channel/pigeon/messaging_page.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MessagingPage(),
+    );
+  }
+}
+
+
+/** pigeon command
+ *flutter pub run pigeon \
+    --input pigeons/messaging.dart \
+    --dart_out lib/pigeon.dart \
+    --kotlin_out ./android/app/src/main/kotlin/com/example/flutter_platform_channel/Pigeon.kt \
+    --java_package "com.example.flutter_platform_channel"
+
+ **/
